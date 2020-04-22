6be2d35ee028f600d9a5ed1343d0ab358daef35b
ngocqui196 <58946367+ngocqui196@users.noreply.github.com>
2020-04-22 10:29:56 +0700

    dfsv

--- java.js
+++ java.js
@@ -53,6 +53,21 @@ let fruit = function (id, name, price, source, image) {
     this.getImage = function () {
         return this.image;
     };
+    this.setId = function (fruitId) {
+        this.id = fruitId
+    };
+    this.setName = function (fruitName) {
+        this.name = fruitName;
+    };
+    this.setPrice = function (fruitPrice) {
+        this.price = fruitPrice
+    };
+    this.setSource = function (fruitSource) {
+        this.source = fruitSource
+    };
+    this.setImage = function (fruitImage) {
+        this.image = fruitImage
+    }
 };
 
 let fruitList = [];
@@ -64,7 +79,13 @@ function addFruit() {
     let fruitImage = document.getElementById("fruitImage").value;
     let fruit1 = new fruit(fruitId, fruitName, fruitPrice, fruitSource, fruitImage);
     fruitList.push(fruit1);
-    loadFullScreen();
+     document.getElementById("fruitId").value='';
+     document.getElementById("fruitName").value='';
+     document.getElementById("fruitPrice").value='';
+     document.getElementById("fruitSource").value='';
+     document.getElementById("fruitImage").value='';
+    document.getElementById("fruitId").focus();
+    loadFullScreen(fruitList);
 }
 
 let row = "";
@@ -75,24 +96,56 @@ function loadFullScreen() {
         row += "<tr><td>" + fruitList[i].getId() + "</td><td>" + fruitList[i].getName()
             + "</td><td>" + fruitList[i].getPrice() + "</td><td>" + fruitList[i].getSource()
             + "</td><td><center><img height='200px' src="+fruitList[i].getImage()+"></center> </td>"
-            + "<td><center><button  onclick='edit(" + i + ")'>Edit</button></center></td>"
+            + "<td><center><button id='btnEdit' onclick='edit(" + i + ")'>Edit</button></center></td>"
             + "<td><center><button  onclick=' fruitDelete(" + i + ")'>Delete</button></center></td></tr>";
         document.getElementById("detailList").innerHTML = row;
     }
 
 }
-
+let ret;
 function edit(index) {
-    let fruit = fruitList[index];
-    fruit.id = document.getElementById("fruitId").value;
-    fruit.name = document.getElementById("fruitName").value;
-    fruit.price = document.getElementById("fruitPrice").value;
-    fruit.source = document.getElementById("fruitSource").value;
-    fruit.image = document.getElementById("fruitImage").value;
-    fruitList[index] = fruit;
-    loadFullScreen();
-}
+    ret = index;
+    document.getElementById("btnEdit").disabled = true;
+    document.getElementById("btnAdd").disabled = false;
 
+    document.product.fruitId.value= fruitList[index].getId();
+    document.product.fruitName.value= fruitList[index].getName();
+    document.product.fruitPrice.value= fruitList[index].getPrice();
+    document.product.fruitSource.value= fruitList[index].getSource();
+    document.product.fruitImage.value= fruitList[index].getImage();
+
+}
+function saveFruit(index) {
+    index = ret ;
+    // Khai báo các biến có giá trị nhập váo trường ô inputt text cho các biến đó
+    let fruitId =document.getElementById("fruitId").value;
+    let fruitName =document.getElementById("fruitName").value;
+    let fruitPrice =document.getElementById("fruitPrice").value;
+    let fruitSource=document.getElementById("fruitSource").value;
+    let fruitImage=document.getElementById("fruitImage").value;
+
+    let createPrice = Number(fruitPrice);
+    if (fruitId == ""||fruitName ==""||fruitPrice == ""||fruitSource =="" || fruitImage == ""||!createPrice){
+        alert("Nhập thiếu thông tin,Hoặc đã sai thông tin");
+    }else {
+        alert("Sửa thành công");
+        fruitList[index].setId(fruitId);
+        fruitList[index].setName(fruitName);
+        fruitList[index].setPrice(fruitPrice);
+        fruitList[index].setSource(fruitSource);
+        fruitList[index].setImage(fruitImage);
+
+        //xóa trống bảng thông tin sinh viên trong trường nhập
+        document.getElementById("fruitId").value='';
+        document.getElementById("fruitName").value='';
+        document.getElementById("fruitPrice").value='';
+        document.getElementById("fruitSource").value='';
+        document.getElementById("fruitImage").value='';
+        document.getElementById("fruitId").focus(); // chỏ chuột vào trường nhập thông tin
+    }
+
+    loadFullScreen(index);
+}
 
 function fruitDelete(index) {
     let del = confirm("Bạn chắc chắn muốn xóa " + fruitList[index]);
